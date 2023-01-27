4d0a4cbf6a948ac043c95c968ca5cb72c4cc40bd
K Fitzsimmons <kafitz22@gmail.com>
2023-01-27 11:25:58 -0500

    Add basic ve subgraph

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,39 +1,45 @@
-export const FACTORY_ADDRESS = '0xd3C1bF5582b5f3029b15bE04a49C65d3226dFB0C'
-export const DFX_MULTISIG_ADDRESS = '0x26f539A0fE189A7f228D7982BF10Bc294FA9070c'
-export const BLACKHOLE_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const FACTORY_ADDRESS = "0xd3C1bF5582b5f3029b15bE04a49C65d3226dFB0C";
+export const DFX_MULTISIG_ADDRESS =
+  "0x26f539A0fE189A7f228D7982BF10Bc294FA9070c";
+export const BLACKHOLE_ADDRESS = "0x0000000000000000000000000000000000000000";
 
-export const ZAP_ADDRESS = '0x64d65e3d70ba0f8812a9d1d7b8b5c51dab78cd15'
-export const ZAP_OPTIMIZED_ADDRESS = '0x607690f749fadb8fb87fc01f6dbdee277eea5fdf'
-export const ZAP_LEGACY_ADDRESS = '0x0e1532173120b81226eaf012c553267c0f5346ff'
+export const ZAP_ADDRESS = "0x64d65e3d70ba0f8812a9d1d7b8b5c51dab78cd15";
+export const ZAP_OPTIMIZED_ADDRESS =
+  "0x607690f749fadb8fb87fc01f6dbdee277eea5fdf";
+export const ZAP_LEGACY_ADDRESS = "0x0e1532173120b81226eaf012c553267c0f5346ff";
 
-export const DFX = '0x888888435FDe8e7d4c54cAb67f206e4199454c60'
-export const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
-export const XSGD = '0x70e8de73ce538da2beed35d14187f6959a8eca96'
-export const CADC = '0xcadc0acd4b445166f12d2c07eac6e2544fbe2eef'
-export const EURS = '0xdb25f211ab05b1c97d595516f45794528a807ad8'
-export const NZDS = '0xda446fad08277b4d2591536f204e018f32b6831c'
-export const TRYB = '0x2c537e5624e4af88a7ae4060c022609376c8d0eb'
-export const XIDR = '0xebf2096e01455108badcbaf86ce30b6e5a72aa52'
-export const EUROC = '0x1abaea1f7c830bd89acc67ec4af516284b1bc33c'
-export const GYEN = '0xc08512927d12348f6620a698105e1baac6ecd911'
+export const DFX = "0x888888435FDe8e7d4c54cAb67f206e4199454c60";
+export const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+export const XSGD = "0x70e8de73ce538da2beed35d14187f6959a8eca96";
+export const CADC = "0xcadc0acd4b445166f12d2c07eac6e2544fbe2eef";
+export const EURS = "0xdb25f211ab05b1c97d595516f45794528a807ad8";
+export const NZDS = "0xda446fad08277b4d2591536f204e018f32b6831c";
+export const TRYB = "0x2c537e5624e4af88a7ae4060c022609376c8d0eb";
+export const XIDR = "0xebf2096e01455108badcbaf86ce30b6e5a72aa52";
+export const EUROC = "0x1abaea1f7c830bd89acc67ec4af516284b1bc33c";
+export const GYEN = "0xc08512927d12348f6620a698105e1baac6ecd911";
 
-export const XSGD_POOL = '0x2bab29a12a9527a179da88f422cdaaa223a90bd5'
-export const CADC_POOL = '0xa6c0cbcaebd93ad3c6c94412ec06aaa37870216d'
-export const EURS_POOL = '0x1a4ffe0dcbdb4d551cfca61a5626afd190731347'
-export const NZDS_POOL = '0xe9669516e09f5710023566458f329cce6437aaac'
-export const TRYB_POOL = '0xc574a613a3900e4314da13eb2287f13689a5b64d'
-export const XIDR_POOL = '0xdd39379ab7c93b9baae29e6ec03795d0bc99a889'
-export const EUROC_POOL = '0x66310ec13f36caf5532c32b4359760592db835ab'
-export const GYEN_POOL = '0x57bc10876febdc57f0abb8c82ffae600eecc03a2'
+export const XSGD_POOL = "0x2bab29a12a9527a179da88f422cdaaa223a90bd5";
+export const CADC_POOL = "0xa6c0cbcaebd93ad3c6c94412ec06aaa37870216d";
+export const EURS_POOL = "0x1a4ffe0dcbdb4d551cfca61a5626afd190731347";
+export const NZDS_POOL = "0xe9669516e09f5710023566458f329cce6437aaac";
+export const TRYB_POOL = "0xc574a613a3900e4314da13eb2287f13689a5b64d";
+export const XIDR_POOL = "0xdd39379ab7c93b9baae29e6ec03795d0bc99a889";
+export const EUROC_POOL = "0x66310ec13f36caf5532c32b4359760592db835ab";
+export const GYEN_POOL = "0x57bc10876febdc57f0abb8c82ffae600eecc03a2";
 
-export const XSGD_STAKING = '0xd52d48db08e8224ef6e2be8f54f3c84e790b1c32'
-export const CADC_STAKING = '0x84bf8151394dcf32146965753b28760550f3d7a8'
-export const EURS_STAKING = '0x5eaaeff69f2ab64d1cc0244fb31b236ca989544f'
-export const NZDS_STAKING = '0xe06fa52e0d2d58fe192285bfa0507f09cdd9824a'
-export const TRYB_STAKING = '0xddb720069fdfe7be2e2883a1c06be0f353f7c4c8'
-export const XIDR_STAKING = '0xe29b7285c1169a9765e2a9bfe74209077bee55d6'
-export const EUROC_STAKING = '0x47b16412c0c9e22362c07ccef8879172ddc06f6d'
-export const GYEN_STAKING = '0x8e41303aaeb500108bf229f6666f9eb6071e18cb'
+export const XSGD_STAKING = "0xd52d48db08e8224ef6e2be8f54f3c84e790b1c32";
+export const CADC_STAKING = "0x84bf8151394dcf32146965753b28760550f3d7a8";
+export const EURS_STAKING = "0x5eaaeff69f2ab64d1cc0244fb31b236ca989544f";
+export const NZDS_STAKING = "0xe06fa52e0d2d58fe192285bfa0507f09cdd9824a";
+export const TRYB_STAKING = "0xddb720069fdfe7be2e2883a1c06be0f353f7c4c8";
+export const XIDR_STAKING = "0xe29b7285c1169a9765e2a9bfe74209077bee55d6";
+export const EUROC_STAKING = "0x47b16412c0c9e22362c07ccef8879172ddc06f6d";
+export const GYEN_STAKING = "0x8e41303aaeb500108bf229f6666f9eb6071e18cb";
 
-export const CRV_CADC_DFXCAD_STAKING = '0x9ca41a2dab3cee15308998868ca644e2e3be5c59'
-export const DFX_CADC_PROXY = '0xfe32747d0251ba92bcb80b6d16c8257ecf25ab1c'
+export const CRV_CADC_DFXCAD_STAKING =
+  "0x9ca41a2dab3cee15308998868ca644e2e3be5c59";
+export const DFX_CADC_PROXY = "0xfe32747d0251ba92bcb80b6d16c8257ecf25ab1c";
+
+export const DFX_DISTRIBUTOR_PROXY =
+  "0x86e8c4e7549fbca7eba1aefbdbc23993f721e5ca";
---
+++ subgraphs/ve/src/dfx-distributor.ts
@@ -0,0 +1,64 @@
+import {
+  RateUpdated as RateUpdatedEvent,
+  Recovered as RecoveredEvent,
+  RewardDistributed as RewardDistributedEvent,
+  UpdateMiningParameters as UpdateMiningParametersEvent,
+} from "../generated/dfxDistributor/dfxDistributor";
+import { Rate, Recovered, RewardDistributed } from "../generated/schema";
+
+export function handleRateUpdated(event: RateUpdatedEvent): void {
+  let entity = new Rate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.rate = event.params._newRate;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleRecovered(event: RecoveredEvent): void {
+  let entity = new Recovered(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.tokenAddress = event.params.tokenAddress;
+  entity.to = event.params.to;
+  entity.amount = event.params.amount;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleRewardDistributed(event: RewardDistributedEvent): void {
+  let entity = new RewardDistributed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.gaugeAddr = event.params.gaugeAddr;
+  entity.rewardTally = event.params.rewardTally;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
+
+export function handleUpdateMiningParameters(
+  event: UpdateMiningParametersEvent
+): void {
+  let entity = new Rate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.rate = event.params.rate;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+}
