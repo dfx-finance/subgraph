c1cc0177233e4e10d61564058d4cf4ec95d69ad1
tommy4241 <tommychan4241@gmail.com>
2023-12-14 23:59:49 +0900

    added hour volume

--- subgraphs/amm-v3/src/core.ts
+++ subgraphs/amm-v3/src/core.ts
@@ -152,6 +152,8 @@ export function handleTrade(event: TradeEvent): void {
   pairHourData.volumeToken0 = pairHourData.volumeToken0.plus(amount0);
   pairHourData.volumeToken1 = pairHourData.volumeToken1.plus(amount1);
   pairHourData.volumeUSD = pairHourData.volumeUSD.plus(amount1USD);
+  pairHourData.hourVolumeToken0 = pairHourData.hourVolumeToken0.plus(amount0);
+  pairHourData.hourVolumeToken1 = pairHourData.hourVolumeToken0.plus(amount1);
 
   pairHourData.save();
 
